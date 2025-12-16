ddb30fb0196b0aeb9b685a0cc1e1cb47a3da3c94
gyqls080813 <124768918+gyqls080813@users.noreply.github.com>
2025-12-16 15:16:40 +0900

    [Silver III] Title: N과 M (3), Time: 2004 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+input_list = [n for n in range(1, N + 1)]
+isused = [False] * N
+
+def solution(step, in_list):
+    if step == M:
+        print(*in_list)
+        return
+    for i in range(N):
+        solution(step + 1, in_list + [input_list[i]])
+        
+solution(0, [])
